7e490c4e0bce1a7991b43812b161fbf80d666a1f
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-04-29 19:25:45 +0300

    Create entity Order

---
+++ src/main/java/com/gr1tEnt/models/Order.java
@@ -0,0 +1,19 @@
+package com.gr1tEnt.models;
+
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Order {
+    private UUID order_id;
+    private UUID customer_id;
+    private LocalDate order_date;
+    private Status status;
+    private double total_amount;
+    private String shipping_address;
+}
