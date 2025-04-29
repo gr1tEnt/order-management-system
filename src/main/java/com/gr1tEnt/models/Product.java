837ecf06dd1bb43f530e63f4f83b23726f98bd6e
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-04-29 19:29:49 +0300

    Create entity Product

---
+++ src/main/java/com/gr1tEnt/models/Product.java
@@ -0,0 +1,17 @@
+package com.gr1tEnt.models;
+
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Product {
+    private UUID product_id;
+    private String product_name;
+    private String product_description;
+    private double price;
+    private int stock_quantity;
+}
