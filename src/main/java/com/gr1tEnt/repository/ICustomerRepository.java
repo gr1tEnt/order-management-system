3bff837f2283e25b7fb786fb2c40d4f218f0ab69
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-04-30 18:23:26 +0300

    Create ICustomerRepository

---
+++ src/main/java/com/gr1tEnt/repository/ICustomerRepository.java
@@ -0,0 +1,19 @@
+package com.gr1tEnt.repository;
+
+import com.gr1tEnt.models.Customer;
+import com.gr1tEnt.models.CustomerDto;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ICustomerRepository {
+    CustomerDto registerCustomer(Customer customer);
+    Optional<Customer> findCustomerById(UUID customerId);
+    Optional<Customer> findCustomerByEmailInternal(String email);
+    Optional<CustomerDto> findCustomerByEmailDto(String email);
+    CustomerDto updateCustomer(UUID customerId, CustomerDto customerDto);
+    List<CustomerDto> findAllCustomers();
+    List<CustomerDto> findAllCustomersWithTotalSpendingAbove(BigDecimal minTotalSpent);
+}
