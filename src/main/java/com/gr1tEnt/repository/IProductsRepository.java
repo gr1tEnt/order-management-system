e13c22fb5a7299ccf4299b91d5eaf7211a5f21c5
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-05-04 19:11:40 +0300

    Expand interface IProductsRepository

--- src/main/java/com/gr1tEnt/repository/IProductsRepository.java
+++ src/main/java/com/gr1tEnt/repository/IProductsRepository.java
@@ -3,7 +3,9 @@ package com.gr1tEnt.repository;
 import com.gr1tEnt.models.Category;
 import com.gr1tEnt.models.Product;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,4 +15,8 @@ public interface IProductsRepository {
     boolean updateStockQuantity(UUID productId, int quantity);
     boolean deleteProductById(UUID productId);
     List<Product> findProductsByCategory(Category category);
+    Map<Category, Long> countProductsByCategory();
+    List<Product> findProductsByPriceRange(BigDecimal minPrice, BigDecimal maxPrice);
+    List<Product> findProductsByNameLike(String namePattern);
+    List<Product> findProductsInStock();
 }
