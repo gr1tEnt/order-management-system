5f344e885548713e83eff45240480b52e049694a
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-05-04 18:05:22 +0300

    Add findProductById method

--- src/main/java/com/gr1tEnt/repository/JdbcProductRepository.java
+++ src/main/java/com/gr1tEnt/repository/JdbcProductRepository.java
@@ -5,6 +5,7 @@ import com.gr1tEnt.models.Product;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
@@ -36,7 +37,32 @@ public class JdbcProductRepository implements IProductsRepository {
 
     @Override
     public Optional<Product> findProductById(UUID productId) {
-        return Optional.empty();
+        String sql = "SELECT product_id, product_name, product_description, price, stock_quantity " +
+                "FROM products " +
+                "WHERE product_id = ?";
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, String.valueOf(productId));
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                Product product = new Product(
+                        UUID.fromString(rs.getString("product_id")),
+                        rs.getString("product_name"),
+                        rs.getString("product_description"),
+                        rs.getDouble("price"),
+                        rs.getInt("stock_quantity"),
+                        Category.valueOf(rs.getString("category"))
+                );
+                return Optional.of(product);
+            } else {
+                return Optional.empty();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
