9090d4c9159b2e2ec01cfab6611847c26238aaf8
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-05-05 10:00:00 +0300

    Add findCustomersWithoutOrders method

--- src/main/java/com/gr1tEnt/repository/JdbcCustomerRepository.java
+++ src/main/java/com/gr1tEnt/repository/JdbcCustomerRepository.java
@@ -145,4 +145,33 @@ public class JdbcCustomerRepository implements ICustomerRepository {
             throw new RuntimeException(e);
         }
     }
+
+    @Override
+    public List<Customer> findCustomersWithoutOrders() {
+        List<Customer> customersWithoutOrders = new ArrayList<>();
+
+        String sql = "SELECT c.customer_id, c.first_name, c.last_name, c.email, c.password_hash, c.address " +
+                "FROM customers c " +
+                "LEFT JOIN orders o " +
+                "ON o.customer_id = c.customer_id " +
+                "WHERE o.order_id IS NULL";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Customer customer = new Customer(
+                        UUID.fromString(rs.getString("customer_id")),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getString("email"),
+                        rs.getString("password_hash"),
+                        rs.getString("address")
+                );
+                customersWithoutOrders.add(customer);
+            }
+            return customersWithoutOrders;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
