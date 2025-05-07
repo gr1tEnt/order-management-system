980a4b2a25f180b5e35361c4cbf24d84c0c55386
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-05-07 19:31:33 +0300

    Final code refactoring for JdbcCustomerRepository

--- src/main/java/com/gr1tEnt/repository/JdbcCustomerRepository.java
+++ src/main/java/com/gr1tEnt/repository/JdbcCustomerRepository.java
@@ -43,20 +43,21 @@ public class JdbcCustomerRepository implements ICustomerRepository {
         try (PreparedStatement stm = conn.prepareStatement(sql)) {
 
             stm.setString(1, String.valueOf(customerId));
-            ResultSet rs = stm.executeQuery();
 
-            if (rs.next()) {
-                Customer customer = new Customer(
-                        UUID.fromString(rs.getString("customer_id")),
-                        rs.getString("first_name"),
-                        rs.getString("last_name"),
-                        rs.getString("email"),
-                        rs.getString("password_hash"),
-                        rs.getString("address")
-                );
-                return Optional.of(customer);
-            } else {
-                return Optional.empty();
+            try (ResultSet rs = stm.executeQuery()) {
+                if (rs.next()) {
+                    Customer customer = new Customer(
+                            UUID.fromString(rs.getString("customer_id")),
+                            rs.getString("first_name"),
+                            rs.getString("last_name"),
+                            rs.getString("email"),
+                            rs.getString("password_hash"),
+                            rs.getString("address")
+                    );
+                    return Optional.of(customer);
+                } else {
+                    return Optional.empty();
+                }
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -68,7 +69,6 @@ public class JdbcCustomerRepository implements ICustomerRepository {
         String sql = "UPDATE customers " +
                 "SET password_hash = ? " +
                 "WHERE customer_id = ?";
-
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, newPassword);
@@ -85,8 +85,8 @@ public class JdbcCustomerRepository implements ICustomerRepository {
         List<Customer> currentCustomers = new ArrayList<>();
         String sql = "SELECT customer_id, first_name, last_name, email, password_hash, address " +
                 "FROM customers";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            ResultSet rs = stmt.executeQuery();
+        try (PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
 
             while (rs.next()) {
                 Customer customer = new Customer(
@@ -115,17 +115,16 @@ public class JdbcCustomerRepository implements ICustomerRepository {
                 "INNER JOIN orders o ON c.customer_id = o.customer_id " +
                 "GROUP BY c.customer_id " +
                 "HAVING SUM(o.total_amount) > ?";
-
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
             stmt.setBigDecimal(1, minTotalSpent);
 
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                matchingCustomers.put(UUID.fromString(rs.getString("customer_id")),
-                        rs.getBigDecimal("total_customer_spending"));
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    matchingCustomers.put(UUID.fromString(rs.getString("customer_id")),
+                            rs.getBigDecimal("total_customer_spending"));
+                }
             }
-
             return matchingCustomers;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -138,6 +137,7 @@ public class JdbcCustomerRepository implements ICustomerRepository {
                 "FROM customers " +
                 "WHERE customer_id = ?";
         try (PreparedStatement stm = conn.prepareStatement(sql)) {
+
             stm.setString(1, String.valueOf(customerId));
 
             return stm.executeUpdate() > 0;
@@ -152,11 +152,10 @@ public class JdbcCustomerRepository implements ICustomerRepository {
 
         String sql = "SELECT c.customer_id, c.first_name, c.last_name, c.email, c.password_hash, c.address " +
                 "FROM customers c " +
-                "LEFT JOIN orders o " +
-                "ON o.customer_id = c.customer_id " +
+                "LEFT JOIN orders o ON o.customer_id = c.customer_id " +
                 "WHERE o.order_id IS NULL";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            ResultSet rs = stmt.executeQuery();
+        try (PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
 
             while (rs.next()) {
                 Customer customer = new Customer(
@@ -187,17 +186,18 @@ public class JdbcCustomerRepository implements ICustomerRepository {
 
             stmt.setString(1, pattern);
 
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                Customer customer = new Customer(
-                        UUID.fromString(rs.getString("customer_id")),
-                        rs.getString("first_name"),
-                        rs.getString("last_name"),
-                        rs.getString("email"),
-                        rs.getString("password_hash"),
-                        rs.getString("address")
-                );
-                matchingCustomers.add(customer);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Customer customer = new Customer(
+                            UUID.fromString(rs.getString("customer_id")),
+                            rs.getString("first_name"),
+                            rs.getString("last_name"),
+                            rs.getString("email"),
+                            rs.getString("password_hash"),
+                            rs.getString("address")
+                    );
+                    matchingCustomers.add(customer);
+                }
             }
             return matchingCustomers;
         } catch (SQLException e) {
@@ -217,17 +217,18 @@ public class JdbcCustomerRepository implements ICustomerRepository {
 
             stmt.setString(1, pattern);
 
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                Customer customer = new Customer(
-                        UUID.fromString(rs.getString("customer_id")),
-                        rs.getString("first_name"),
-                        rs.getString("last_name"),
-                        rs.getString("email"),
-                        rs.getString("password_hash"),
-                        rs.getString("address")
-                );
-                matchingCustomers.add(customer);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Customer customer = new Customer(
+                            UUID.fromString(rs.getString("customer_id")),
+                            rs.getString("first_name"),
+                            rs.getString("last_name"),
+                            rs.getString("email"),
+                            rs.getString("password_hash"),
+                            rs.getString("address")
+                    );
+                    matchingCustomers.add(customer);
+                }
             }
             return matchingCustomers;
         } catch (SQLException e) {
@@ -241,25 +242,25 @@ public class JdbcCustomerRepository implements ICustomerRepository {
 
         String sql = "SELECT c.customer_id, c.first_name, c.last_name, c.email, c.password_hash, c.address " +
                 "FROM customers c " +
-                "INNER JOIN orders o " +
-                "ON c.customer_id = o.customer_id " +
+                "INNER JOIN orders o ON c.customer_id = o.customer_id " +
                 "GROUP BY c.customer_id, c.first_name, c.last_name, c.email, c.password_hash, c.address" +
                 "HAVING COUNT(o.order_id) > ?";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
             stmt.setInt(1, minOrderCount);
 
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                Customer customer = new Customer(
-                        UUID.fromString(rs.getString("customer_id")),
-                        rs.getString("first_name"),
-                        rs.getString("last_name"),
-                        rs.getString("email"),
-                        rs.getString("password_hash"),
-                        rs.getString("address")
-                );
-                customers.add(customer);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Customer customer = new Customer(
+                            UUID.fromString(rs.getString("customer_id")),
+                            rs.getString("first_name"),
+                            rs.getString("last_name"),
+                            rs.getString("email"),
+                            rs.getString("password_hash"),
+                            rs.getString("address")
+                    );
+                    customers.add(customer);
+                }
             }
             return customers;
         } catch (SQLException e) {
@@ -273,25 +274,25 @@ public class JdbcCustomerRepository implements ICustomerRepository {
 
         String sql = "SELECT c.customer_id, c.first_name, c.last_name, c.email, c.password_hash, c.address " +
                 "FROM customers c " +
-                "INNER JOIN orders o " +
-                "ON c.customer_id = o.customer_id " +
+                "INNER JOIN orders o ON c.customer_id = o.customer_id " +
                 "GROUP BY c.customer_id, c.first_name, c.last_name, c.email, c.password_hash, c.address " +
                 "HAVING AVG(o.total_amount) < ?";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
             stmt.setBigDecimal(1, maxAverageValue);
 
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                Customer customer = new Customer(
-                        UUID.fromString(rs.getString("customer_id")),
-                        rs.getString("first_name"),
-                        rs.getString("last_name"),
-                        rs.getString("email"),
-                        rs.getString("password_hash"),
-                        rs.getString("address")
-                );
-                customers.add(customer);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Customer customer = new Customer(
+                            UUID.fromString(rs.getString("customer_id")),
+                            rs.getString("first_name"),
+                            rs.getString("last_name"),
+                            rs.getString("email"),
+                            rs.getString("password_hash"),
+                            rs.getString("address")
+                    );
+                    customers.add(customer);
+                }
             }
             return customers;
         } catch (SQLException e) {
@@ -305,17 +306,17 @@ public class JdbcCustomerRepository implements ICustomerRepository {
 
         String sql = "SELECT c.customer_id, AVG(o.total_amount) AS average_order_value " +
                 "FROM customers c " +
-                "INNER JOIN orders o " +
-                "ON c.customer_id = o.customer_id " +
+                "INNER JOIN orders o ON c.customer_id = o.customer_id " +
                 "GROUP BY c.customer_id ";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            ResultSet rs = stmt.executeQuery();
 
-            while (rs.next()) {
-                customersWithAverageOrderAmount.put(
-                        UUID.fromString(rs.getString("customer_id")),
-                        rs.getBigDecimal("average_order_value")
-                );
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    customersWithAverageOrderAmount.put(
+                            UUID.fromString(rs.getString("customer_id")),
+                            rs.getBigDecimal("average_order_value")
+                    );
+                }
             }
             return customersWithAverageOrderAmount;
         } catch (SQLException e) {
@@ -334,13 +335,14 @@ public class JdbcCustomerRepository implements ICustomerRepository {
                 "GROUP BY c.customer_id";
 
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            ResultSet rs = stmt.executeQuery();
 
-            while (rs.next()) {
-                ordersPerCustomer.put(
-                        UUID.fromString(rs.getString("customer_id")),
-                        rs.getLong("ordersQuantity")
-                );
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    ordersPerCustomer.put(
+                            UUID.fromString(rs.getString("customer_id")),
+                            rs.getLong("ordersQuantity")
+                    );
+                }
             }
             return ordersPerCustomer;
         } catch (SQLException e) {
