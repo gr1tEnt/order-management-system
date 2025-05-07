c5118a8e0dfe6dc0a812de045fbc43f9c92065d3
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-05-07 20:16:14 +0300

    Final code refactoring for JdbcProductRepository

--- src/main/java/com/gr1tEnt/repository/JdbcProductRepository.java
+++ src/main/java/com/gr1tEnt/repository/JdbcProductRepository.java
@@ -38,29 +38,29 @@ public class JdbcProductRepository implements IProductsRepository {
 
     @Override
     public Optional<Product> findProductById(UUID productId) {
-        String sql = "SELECT product_id, product_name, product_description, price, stock_quantity " +
+        String sql = "SELECT product_id, product_name, product_description, price, stock_quantity, category " +
                 "FROM products " +
                 "WHERE product_id = ?";
 
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, String.valueOf(productId));
-            ResultSet rs = stmt.executeQuery();
 
-            if (rs.next()) {
-                Product product = new Product(
-                        UUID.fromString(rs.getString("product_id")),
-                        rs.getString("product_name"),
-                        rs.getString("product_description"),
-                        rs.getBigDecimal("price"),
-                        rs.getInt("stock_quantity"),
-                        ProductCategory.valueOf(rs.getString("category"))
-                );
-                return Optional.of(product);
-            } else {
-                return Optional.empty();
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    Product product = new Product(
+                            UUID.fromString(rs.getString("product_id")),
+                            rs.getString("product_name"),
+                            rs.getString("product_description"),
+                            rs.getBigDecimal("price"),
+                            rs.getInt("stock_quantity"),
+                            ProductCategory.valueOf(rs.getString("category").toUpperCase())
+                    );
+                    return Optional.of(product);
+                } else {
+                    return Optional.empty();
+                }
             }
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -68,10 +68,11 @@ public class JdbcProductRepository implements IProductsRepository {
 
     @Override
     public boolean updateStockQuantity(UUID productId, int quantity) {
-        String sql = "UPDATE product " +
+        String sql = "UPDATE products " +
                 "SET stock_quantity = ? " +
                 "WHERE product_id = ?";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
             stmt.setInt(1, quantity);
             stmt.setString(2, String.valueOf(productId));
 
@@ -87,6 +88,7 @@ public class JdbcProductRepository implements IProductsRepository {
                 "FROM products " +
                 "WHERE product_id = ?";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
             stmt.setString(1, String.valueOf(productId));
 
             return stmt.executeUpdate() > 0;
@@ -98,24 +100,26 @@ public class JdbcProductRepository implements IProductsRepository {
     @Override
     public List<Product> findProductsByCategory(ProductCategory category) {
         List<Product> currentProducts = new ArrayList<>();
+
         String sql = "SELECT product_id, product_name, product_description, price, stock_quantity, category " +
                 "FROM products " +
                 "WHERE category = ?";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
             stmt.setString(1, String.valueOf(category));
 
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                Product product = new Product(
-                        UUID.fromString(rs.getString("product_id")),
-                        rs.getString("product_name"),
-                        rs.getString("product_description"),
-                        rs.getBigDecimal("price"),
-                        rs.getInt("stock_quantity"),
-                        ProductCategory.valueOf(rs.getString("category"))
-                );
-                currentProducts.add(product);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Product product = new Product(
+                            UUID.fromString(rs.getString("product_id")),
+                            rs.getString("product_name"),
+                            rs.getString("product_description"),
+                            rs.getBigDecimal("price"),
+                            rs.getInt("stock_quantity"),
+                            ProductCategory.valueOf(rs.getString("category").toUpperCase())
+                    );
+                    currentProducts.add(product);
+                }
             }
             return currentProducts;
         } catch (SQLException e) {
@@ -131,16 +135,15 @@ public class JdbcProductRepository implements IProductsRepository {
                 "FROM products " +
                 "GROUP BY category " +
                 "ORDER BY COUNT(product_id) DESC";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            ResultSet rs = stmt.executeQuery();
+        try (PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
 
             while (rs.next()) {
                 groupedByCategory.put(
-                        ProductCategory.valueOf(rs.getString("category")),
+                        ProductCategory.valueOf(rs.getString("category").toUpperCase()),
                         rs.getLong("quantity")
                 );
             }
-
             return groupedByCategory;
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -153,24 +156,24 @@ public class JdbcProductRepository implements IProductsRepository {
 
         String sql = "SELECT product_id, product_name, product_description, price, stock_quantity, category " +
                 "FROM products " +
-                "WHERE price >= ? AND price =< ?";
+                "WHERE price >= ? AND price <= ?";
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setBigDecimal(1, minPrice);
             stmt.setBigDecimal(2, maxPrice);
 
-            ResultSet rs = stmt.executeQuery();
-
-            while (rs.next()) {
-                Product product = new Product(
-                        UUID.fromString(rs.getString("product_id")),
-                        rs.getString("product_name"),
-                        rs.getString("product_description"),
-                        rs.getBigDecimal("price"),
-                        rs.getInt("stock_quantity"),
-                        ProductCategory.valueOf(rs.getString("category"))
-                );
-                products.add(product);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Product product = new Product(
+                            UUID.fromString(rs.getString("product_id")),
+                            rs.getString("product_name"),
+                            rs.getString("product_description"),
+                            rs.getBigDecimal("price"),
+                            rs.getInt("stock_quantity"),
+                            ProductCategory.valueOf(rs.getString("category").toUpperCase())
+                    );
+                    products.add(product);
+                }
             }
             return products;
         } catch (SQLException e) {
@@ -190,17 +193,18 @@ public class JdbcProductRepository implements IProductsRepository {
 
             stmt.setString(1, pattern);
 
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                Product product = new Product(
-                        UUID.fromString(rs.getString("product_id")),
-                        rs.getString("product_name"),
-                        rs.getString("product_description"),
-                        rs.getBigDecimal("price"),
-                        rs.getInt("stock_quantity"),
-                        ProductCategory.valueOf(rs.getString("category"))
-                );
-                products.add(product);
+            try (ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    Product product = new Product(
+                            UUID.fromString(rs.getString("product_id")),
+                            rs.getString("product_name"),
+                            rs.getString("product_description"),
+                            rs.getBigDecimal("price"),
+                            rs.getInt("stock_quantity"),
+                            ProductCategory.valueOf(rs.getString("category").toUpperCase())
+                    );
+                    products.add(product);
+                }
             }
             return products;
         } catch (SQLException e) {
@@ -215,8 +219,9 @@ public class JdbcProductRepository implements IProductsRepository {
         String sql = "SELECT product_id, product_name, product_description, price, stock_quantity, category " +
                 "FROM products " +
                 "WHERE stock_quantity > 0";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            ResultSet rs = stmt.executeQuery();
+        try (PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
             while (rs.next()) {
                 Product product = new Product(
                         UUID.fromString(rs.getString("product_id")),
@@ -224,7 +229,7 @@ public class JdbcProductRepository implements IProductsRepository {
                         rs.getString("product_description"),
                         rs.getBigDecimal("price"),
                         rs.getInt("stock_quantity"),
-                        ProductCategory.valueOf(rs.getString("category"))
+                        ProductCategory.valueOf(rs.getString("category").toUpperCase())
                 );
                 products.add(product);
             }
@@ -241,11 +246,11 @@ public class JdbcProductRepository implements IProductsRepository {
         String sql = "SELECT category, SUM(stock_quantity) AS quantity " +
                 "FROM products " +
                 "GROUP BY category";
-        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            ResultSet rs = stmt.executeQuery();
+        try (PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
 
             while (rs.next()) {
-                quantityPerCategory.put(ProductCategory.valueOf(rs.getString("category")),
+                quantityPerCategory.put(ProductCategory.valueOf(rs.getString("category").toUpperCase()),
                 rs.getInt("quantity"));
             }
             return quantityPerCategory;
