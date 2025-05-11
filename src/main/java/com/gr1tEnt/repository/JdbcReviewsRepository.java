cdce316a6dc16ed1b1a91292044860cd29ecb8aa
Bohdan Vylyvanyi <bodyaviliv@gmail.com>
2025-05-11 11:15:07 +0300

    Add findReviewById method for JdbcReviewsRepository

--- src/main/java/com/gr1tEnt/repository/JdbcReviewsRepository.java
+++ src/main/java/com/gr1tEnt/repository/JdbcReviewsRepository.java
@@ -4,8 +4,11 @@ import com.gr1tEnt.models.Review;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
 
 public class JdbcReviewsRepository implements IReviewsRepository {
     private final Connection conn;
@@ -32,4 +35,34 @@ public class JdbcReviewsRepository implements IReviewsRepository {
             throw new RuntimeException(e);
         }
     }
+
+    @Override
+    public Optional<Review> findReviewById(UUID reviewId) {
+        String sql = "SELECT review_id, product_id, customer_id, rating, comment_text, review_date " +
+                "FROM reviews " +
+                "WHERE review_id = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, String.valueOf(reviewId));
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    Review review = new Review(
+                            UUID.fromString(rs.getString("review_id")),
+                            UUID.fromString(rs.getString("product_id")),
+                            UUID.fromString(rs.getString("customer_id")),
+                            rs.getInt("rating"),
+                            rs.getString("comment_text"),
+                            rs.getObject("review_date", Instant.class)
+                    );
+                    return Optional.of(review);
+                } else {
+                    return Optional.empty();
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
